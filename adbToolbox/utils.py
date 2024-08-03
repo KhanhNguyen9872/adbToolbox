c6b4385a71c289eb67585e9ccb5ab3fccbf97b3d
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-08-03 11:44:23 +0700

    add adbPair, adbConnect, adbDisconnect, add more logging

--- adbToolbox/utils.py
+++ adbToolbox/utils.py
@@ -1,11 +1,12 @@
 import subprocess
 import sys
 import os
+import time
 import logging
 import cv2
 import numpy as np
 
-# logging.disable(logging.WARNING)
+debug = True
 
 if os.name == 'nt':
     adb = ".\\tools\\adb.exe"
@@ -13,11 +14,14 @@ else:
     adb = "./tools/adb.exe"
 
 class device:
-    def __init__(self, id: str) -> None:
+    def __init__(self, id: str, model: str = "") -> None:
         self.id = id
+        self.model = model
         return None
 
     def __repr__(self) -> str:
+        if self.model:
+            return "{} ({})".format(self.id, self.model)
         return self.id
 
     def __call__(self) -> None:
@@ -26,58 +30,66 @@ class device:
 
     def shell(self, s: str) -> str:
         if not self.id:
-            logging.error("device is undefined")
+            raise TypeError("device is undefined")
         cmd = '{adb} "-s" "{device}" "shell" {cmd}'.format(adb = adb, device = self.id, cmd = s)
         result = subprocess.getoutput(cmd)
         if "device '{device}' not found".format(device = self.id) in result:
-            logging.error(result)
-            return ""
+            raise TypeError(result)
+
         return result
 
     def capture(self) -> bytes:
+        global debug
+        __time = time.time()
         cmd = '{adb} "-s" "{device}" "exec-out" "screencap" "-p"'.format(adb = adb, device = self.id)
         result = subprocess.check_output(cmd, shell=True)
         if result:
-            logging.warning("{device}: success capture device (size = {size})".format(device = self.id, size = len(result)))
+            logging.warning("{device}: success capture device (size = {size} kb) in {sec}".format(device = self.id, size = round(len(result) / 1024, 3), sec = str(round(time.time() - __time, 3)) + " sec"))
         return result
 
     def input(self, type: str, x: int, y: int) -> None:
+        global debug
         if (x == 0) and (y == 0):
             return None
 
         cmd = '"input" "{type}" "{x}" "{y}"'.format(type = type, x = x, y = y)
-        logging.warning("{device}: {type} {x}, {y}".format(device = self.id, type = type, x = x, y = y))
+        logging.warning("{device}: {type} (x = {x}, y = {y})".format(device = self.id, type = type, x = x, y = y))
         self.shell(cmd)
         return None
 
-    def findXY(self, path_img):
-        img = self.capture()
+    def findXY(self, path_img: str):
+        try:
+            img = self.capture()
 
-        main_image_array = np.frombuffer(img, np.uint8)
+            main_image_array = np.frombuffer(img, np.uint8)
 
-        main_image = cv2.imdecode(main_image_array, cv2.IMREAD_COLOR)
-        template_image = cv2.imread(path_img)
+            main_image = cv2.imdecode(main_image_array, cv2.IMREAD_COLOR)
+            template_image = cv2.imread(path_img)
 
-        main_gray = cv2.cvtColor(main_image, cv2.COLOR_BGR2GRAY)
-        template_gray = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
+            main_gray = cv2.cvtColor(main_image, cv2.COLOR_BGR2GRAY)
+            template_gray = cv2.cvtColor(template_image, cv2.COLOR_BGR2GRAY)
 
-        w, h = template_gray.shape[::-1]
+            w, h = template_gray.shape[::-1]
 
-        res = cv2.matchTemplate(main_gray, template_gray, cv2.TM_CCOEFF_NORMED)
+            res = cv2.matchTemplate(main_gray, template_gray, cv2.TM_CCOEFF_NORMED)
 
-        threshold = 0.7
+            threshold = 0.75
 
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-        if max_val >= threshold:
-            x, y = max_loc
-            logging.warning("{device}: found '{file}' [{x}, {y}]".format(file = path_img, x = x, y = y, device = self.id))
-            return x, y
-        else:
-            logging.warning("{device}: not found '{file}'".format(file = path_img, device = self.id))
-            return 0, 0
+            if max_val >= threshold:
+                x, y = max_loc
+                logging.warning("{device}: found '{file}' (x = {x}, y = {y})".format(file = path_img, x = x, y = y, device = self.id))
+                return x, y
+            else:
+                logging.warning("{device}: not found '{file}'".format(file = path_img, device = self.id))
+                return 0, 0
+        except Exception as ex:
+            raise ex
+        return 0, 0
 
 def getDevices() -> list:
+    logging.warning("adb get devices....")
     cmd = "{adb} devices".format(adb = adb)
     result = subprocess.getoutput(cmd)
     devices = []
@@ -89,12 +101,44 @@ def getDevices() -> list:
                     continue
                 i = i.split('\t')
                 if i[1] == 'device' or i[1] == 'offline':
-                    devices.append(device(i[0]))
+                    cmd = '{adb} "-s" "{id}" "shell" "getprop" "ro.product.model"'.format(adb = adb, id = i[0])
+                    devices.append(device(i[0], subprocess.getoutput(cmd)))
     else:
         raise MemoryError("adb not working")
+    logging.warning("found {} device".format(len(devices)))
     return devices
 
-def kill_server() -> None:
+def killServer() -> None:
+    logging.warning("adb kill server...")
     cmd = '{adb} kill-server'.format(adb = adb)
+    subprocess.getoutput(cmd)
+    cmd = "taskkill /f /im adb.exe"
+    subprocess.getoutput(cmd)
+    return None
+
+def adbPair(ipAddress: str, pairCode: int) -> None:
+    logging.warning("pairing to {ip}".format(ip = ipAddress))
+    cmd = '{adb} "pair" "{ip}"'.format(adb = adb, ip = ipAddress)
+    
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)    
+    stdout_ = p.communicate(input=str(pairCode).encode('utf8'))[0]
+    if 'Successfully paired to {ip}'.format(ip = ipAddress).encode('utf8') in stdout_:
+        logging.warning("paired to {ip}".format(ip = ipAddress))
+    else:
+        logging.warning("cannot pair to {ip}".format(ip = ipAddress))
+    return None
+
+def adbConnect(ipAddress: str) -> None:
+    logging.warning("connecting to {ip}".format(ip = ipAddress))
+    cmd = '{adb} "connect" "{ip}"'.format(adb = adb, ip = ipAddress)
+    p = subprocess.getoutput(cmd)
+    if "connected to {ip}".format(ip = ipAddress) in p:
+        logging.warning("connected to {ip}".format(ip = ipAddress))
+    else:
+        logging.warning("cannot connect to {ip}".format(ip = ipAddress))
+    return None
+
+def adbDisconnect() -> None:
+    cmd = '{adb} disconnect'.format(adb = adb)
     subprocess.getoutput(cmd)
     return None
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import adbToolbox
 
+adbToolbox.killServer()
+# adbToolbox.adbPair("192.168.1.8:40775", "343981")
+# adbToolbox.adbConnect("192.168.1.8:39823")
+
 devices = adbToolbox.getDevices()
+
+print("All devices:", devices)
 
 for device in devices:
     x, y = device.findXY('target.png')
