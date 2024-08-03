6469f10a203ba0d6e7262028f3148f6fa4980878
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-08-03 15:36:10 +0700

    change to class Control, add inputText, keyEvent

--- adbToolbox/utils.py
+++ adbToolbox/utils.py
@@ -6,17 +6,149 @@ import logging
 import cv2
 import numpy as np
 
-debug = True
+keyCode = {
+    "KEYCODE_UNKNOWN": 0,
+    "KEYCODE_MENU": 1,
+    "KEYCODE_SOFT_RIGHT": 2,
+    "KEYCODE_HOME": 3,
+    "KEYCODE_BACK": 4,
+    "KEYCODE_CALL": 5,
+    "KEYCODE_ENDCALL": 6,
+    "KEYCODE_0": 7,
+    "KEYCODE_1": 8,
+    "KEYCODE_2": 9,
+    "KEYCODE_3": 10,
+    "KEYCODE_4": 11,
+    "KEYCODE_5": 12,
+    "KEYCODE_6": 13,
+    "KEYCODE_7": 14,
+    "KEYCODE_8": 15,
+    "KEYCODE_9": 16,
+    "KEYCODE_STAR": 17,
+    "KEYCODE_POUND": 18,
+    "KEYCODE_DPAD_UP": 19,
+    "KEYCODE_DPAD_DOWN": 20,
+    "KEYCODE_DPAD_LEFT": 21,
+    "KEYCODE_DPAD_RIGHT": 22,
+    "KEYCODE_DPAD_CENTER": 23,
+    "KEYCODE_VOLUME_UP": 24,
+    "KEYCODE_VOLUME_DOWN": 25,
+    "KEYCODE_POWER": 26,
+    "KEYCODE_CAMERA": 27,
+    "KEYCODE_CLEAR": 28,
+    "KEYCODE_A": 29,
+    "KEYCODE_B": 30,
+    "KEYCODE_C": 31,
+    "KEYCODE_D": 32,
+    "KEYCODE_E": 33,
+    "KEYCODE_F": 34,
+    "KEYCODE_G": 35,
+    "KEYCODE_H": 36,
+    "KEYCODE_I": 37,
+    "KEYCODE_J": 38,
+    "KEYCODE_K": 39,
+    "KEYCODE_L": 40,
+    "KEYCODE_M": 41,
+    "KEYCODE_N": 42,
+    "KEYCODE_O": 43,
+    "KEYCODE_P": 44,
+    "KEYCODE_Q": 45,
+    "KEYCODE_R": 46,
+    "KEYCODE_S": 47,
+    "KEYCODE_T": 48,
+    "KEYCODE_U": 49,
+    "KEYCODE_V": 50,
+    "KEYCODE_W": 51,
+    "KEYCODE_X": 52,
+    "KEYCODE_Y": 53,
+    "KEYCODE_Z": 54,
+    "KEYCODE_COMMA": 55,
+    "KEYCODE_PERIOD": 56,
+    "KEYCODE_ALT_LEFT": 57,
+    "KEYCODE_ALT_RIGHT": 58,
+    "KEYCODE_SHIFT_LEFT": 59,
+    "KEYCODE_SHIFT_RIGHT": 60,
+    "KEYCODE_TAB": 61,
+    "KEYCODE_SPACE": 62,
+    "KEYCODE_SYM": 63,
+    "KEYCODE_EXPLORER": 64,
+    "KEYCODE_ENVELOPE": 65,
+    "KEYCODE_ENTER": 66,
+    "KEYCODE_DEL": 67,
+    "KEYCODE_GRAVE": 68,
+    "KEYCODE_MINUS": 69,
+    "KEYCODE_EQUALS": 70,
+    "KEYCODE_LEFT_BRACKET": 71,
+    "KEYCODE_RIGHT_BRACKET": 72,
+    "KEYCODE_BACKSLASH": 73,
+    "KEYCODE_SEMICOLON": 74,
+    "KEYCODE_APOSTROPHE": 75,
+    "KEYCODE_SLASH": 76,
+    "KEYCODE_AT": 77,
+    "KEYCODE_NUM": 78,
+    "KEYCODE_HEADSETHOOK": 79,
+    "KEYCODE_FOCUS": 80,
+    "KEYCODE_PLUS": 81,
+    "KEYCODE_MENU": 82,
+    "KEYCODE_NOTIFICATION": 83,
+    "KEYCODE_SEARCH": 84,
+    "TAG_LAST_KEYCODE": 85
+}
 
 if os.name == 'nt':
     adb = ".\\tools\\adb.exe"
 else:
     adb = "./tools/adb.exe"
+    
+class Control:
+    def __init__(self, id: str, model: str = "", shell: object = None):
+        self.id = id
+        self.model = model
+        self.shell = shell
+        return None
+    
+    def tap(self, x: int, y: int) -> None:
+        if (x == 0) and (y == 0):
+            return None
 
-class device:
+        cmd = '"input" "tap" "{x}" "{y}"'.format(type = type, x = x, y = y)
+        logging.warning("{device}: tap (x = {x}, y = {y})".format(device = self.id, x = x, y = y))
+        self.shell(cmd)
+        return None
+    
+    def swipe(self) -> None:
+        
+        return None
+
+    def keyEvent(self, event_code: int) -> None:
+        if event_code:
+            try:
+                int(event_code)
+            except ValueError:
+                raise ValueError("wrong keyevent, found [{}]".format(event_code))
+            cmd = '"input" "keyevent" "{event}"'.format(type = type, event = event_code)
+            logging.warning("{device}: keyevent ({event})".format(device = self.id, event = event_code))
+            self.shell(cmd)
+        return None
+
+    def inputText(self, text: str) -> None:
+        if text:
+            logging.warning("{device}: text ({text})".format(device = self.id, text = text))
+            text = text.split(' ')
+            for i in range(0, len(text), 1):
+                t = text[i]
+                cmd = '"input" "text" {text}'.format(type = type, text = t)
+                self.shell(cmd)
+                
+                if not (i == len(text) - 1):
+                    self.keyEvent(62)
+        return None
+
+class Device:
     def __init__(self, id: str, model: str = "") -> None:
         self.id = id
         self.model = model
+        self.Control = Control(id, model, self.shell)
         return None
 
     def __repr__(self) -> str:
@@ -39,25 +171,14 @@ class device:
         return result
 
     def capture(self) -> bytes:
-        global debug
         __time = time.time()
         cmd = '{adb} "-s" "{device}" "exec-out" "screencap" "-p"'.format(adb = adb, device = self.id)
         result = subprocess.check_output(cmd, shell=True)
         if result:
             logging.warning("{device}: success capture device (size = {size} kb) in {sec}".format(device = self.id, size = round(len(result) / 1024, 3), sec = str(round(time.time() - __time, 3)) + " sec"))
         return result
-
-    def input(self, type: str, x: int, y: int) -> None:
-        global debug
-        if (x == 0) and (y == 0):
-            return None
-
-        cmd = '"input" "{type}" "{x}" "{y}"'.format(type = type, x = x, y = y)
-        logging.warning("{device}: {type} (x = {x}, y = {y})".format(device = self.id, type = type, x = x, y = y))
-        self.shell(cmd)
-        return None
-
-    def findXY(self, path_img: str):
+        
+    def findXY(self, path_img: str) -> list:
         try:
             img = self.capture()
 
@@ -79,14 +200,16 @@ class device:
 
             if max_val >= threshold:
                 x, y = max_loc
+                x = x + 15
+                y = y + 15
                 logging.warning("{device}: found '{file}' (x = {x}, y = {y})".format(file = path_img, x = x, y = y, device = self.id))
-                return x, y
+                return [x, y]
             else:
                 logging.warning("{device}: not found '{file}'".format(file = path_img, device = self.id))
-                return 0, 0
+                return [0, 0]
         except Exception as ex:
             raise ex
-        return 0, 0
+        return [0, 0]
 
 def getDevices() -> list:
     logging.warning("adb get devices....")
@@ -102,7 +225,7 @@ def getDevices() -> list:
                 i = i.split('\t')
                 if i[1] == 'device' or i[1] == 'offline':
                     cmd = '{adb} "-s" "{id}" "shell" "getprop" "ro.product.model"'.format(adb = adb, id = i[0])
-                    devices.append(device(i[0], subprocess.getoutput(cmd)))
+                    devices.append(Device(i[0], subprocess.getoutput(cmd)))
     else:
         raise MemoryError("adb not working")
     logging.warning("found {} device".format(len(devices)))
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import adbToolbox
+import time
 
 adbToolbox.killServer()
+
 # adbToolbox.adbPair("192.168.1.8:40775", "343981")
 # adbToolbox.adbConnect("192.168.1.8:39823")
 
@@ -9,5 +11,16 @@ devices = adbToolbox.getDevices()
 print("All devices:", devices)
 
 for device in devices:
-    x, y = device.findXY('target.png')
-    device.input("tap", x, y)
+    x, y = device.findXY('1.png')
+    device.Control.tap(x, y)
+    
+    time.sleep(1)
+    
+    x, y = device.findXY('2.png')
+    device.Control.tap(x, y)
+    
+    time.sleep(1)
+    
+    device.Control.inputText("hello world!")
+    
+    device.Control.keyEvent(adbToolbox.keyCode["KEYCODE_ENTER"])
